3d8e0c6d9f6e7291f432ead9e27e9afe6416dbad
Broxix <ITN250019@qualifizierung.at>
2023-10-29 13:12:01 +0100

    Grundgerüst fertig, inhalt und bugs müssen

--- js/script.js
+++ js/script.js
@@ -24,6 +24,11 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+            //aktiv sektion für animationen scrollIn
+            sec.classList.add('show-animate');
+        }
+        else {
+            sec.classList.remove('show-animate');
         }
     });
 
@@ -36,7 +41,9 @@ menuIcon.classList.remove('bx-x');
 navbar.classList.remove('active');
 
 // animation footer on scroll
+let footer = document.querySelector('footer');
 
+footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
 /*
 const circleSpin = document.getElementById("circle-spin");
