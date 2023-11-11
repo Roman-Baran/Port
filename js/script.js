51ca087bd15deaaabdeae03dbeda44081ac7f886
Broxix <ITN250019@qualifizierung.at>
2023-11-11 12:17:43 +0100

    Mail test

--- js/script.js
+++ js/script.js
@@ -57,3 +57,103 @@ requestAnimationFrame(spin);
 }
 requestAnimationFrame(spin);
 */
+/*  --------------------TypeScript---------------------
+document.addEventListener('DOMContentLoaded', () => {
+    const contactForm = document.getElementById('contact-form') as HTMLFormElement;
+
+    contactForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const name = (document.getElementById('name') as HTMLInputElement).value;
+        const email = (document.getElementById('email') as HTMLInputElement).value;
+        const phone = (document.getElementById('phone') as HTMLInputElement).value;
+        const subject = (document.getElementById('subject') as HTMLInputElement).value;
+        const message = (document.getElementById('message') as HTMLTextAreaElement).value;
+
+        if (validateForm(name, email, phone, subject, message)) {
+            // Sende an den Server oder führe andere Aktionen durch.
+            // fetch() oder AJAX-Bibliothek verwenden, um eine POST-Anfrage an das Backend zu senden.
+        }
+    });
+
+    function validateForm(name: string, email: string, phone: string, subject: string, message: string): boolean {
+        // Validierungslogik
+        // Überprüfe, Validierungskriterien entsprechen.
+        // true wenn das Formular gültig ist. Andernfalls Fehlermeldung-gib false zurück.
+
+        // Überprüfe, E-Mail-Adresse ein gültiges Format hat, reguläre Expression
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        if (!emailPattern.test(email)) {
+            alert('Bitte geben Sie eine gültige E-Mail-Adresse ein.');
+            return false;
+        }
+
+        //weitere Validierungsprüfungen für Name, Telefon, Betreff und Nachricht hinzu.
+
+        return true; // Formular ist gültig.
+    }
+}); */
+document.addEventListener('DOMContentLoaded', () => {
+    const contactForm = document.getElementById('contact-form');
+
+    contactForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const phone = document.getElementById('phone').value;
+        const subject = document.getElementById('subject').value;
+        const message = document.getElementById('message').value;
+
+        if (validateForm(name, email, phone, subject, message)) {
+            // Formulardaten an Server
+            // fetch() oder AJAX-Bibliothek verwenden
+
+            // fetch():
+            fetch('server-endpunkt', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name,
+                    email,
+                    phone,
+                    subject,
+                    message
+                })
+            })
+            .then(response => {
+                if (response.ok) {
+                    // erfolgreich gesendet
+                    alert('Vielen Dank für deine Nachricht.');
+                    contactForm.reset();
+                } else {
+                    // Fehler
+                    alert('Fehler beim Senden des Formulars. Bitte versuche es später erneut.');
+                }
+            })
+            .catch(error => {
+                console.error(error);
+                alert('Fehler beim Senden des Formulars. Bitte versuche es später erneut.');
+            });
+        }
+    });
+
+    function validateForm(name, email, phone, subject, message) {
+        // Hier Validierungslogik implementieren
+        // Überprüfe
+        // Gib true, wenn Formular gültig, Fehlermeldung = false
+
+        // E-Mail-Adresse ein gültiges Format? reguläre Expression
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        if (!emailPattern.test(email)) {
+            alert('Bitte geben Sie eine gültige E-Mail-Adresse ein.');
+            return false;
+        }
+
+        // Für weitere Validierungsprüfungen
+
+        return true; // Formular ist gültig.
+    }
+});
